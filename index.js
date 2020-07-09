139b0283e55432bbf40e114e87a3f02eac74e79f
Isaac Rowntree <isaac@rowntree.me>
2020-07-09 16:01:19 +1000

    bump: lint fix

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 // Pseudocode
 
 import startMatch from './lib/tennis.js';
@@ -30,26 +32,26 @@ import startMatch from './lib/tennis.js';
 
 // Javascript variant of above
 
-const match = startMatch("player 1", "player 2");
-match.pointWonBy("player 1");
-match.pointWonBy("player 2");
+const match = startMatch('player 1', 'player 2');
+match.pointWonBy('player 1');
+match.pointWonBy('player 2');
 // this will return "0-0, 15-15"
 console.log(match.score());
 
-match.pointWonBy("player 1");
-match.pointWonBy("player 1");
+match.pointWonBy('player 1');
+match.pointWonBy('player 1');
 // this will return "0-0, 40-15"
 console.log(match.score());
 
-match.pointWonBy("player 2");
-match.pointWonBy("player 2");
+match.pointWonBy('player 2');
+match.pointWonBy('player 2');
 // this will return "0-0, Deuce"
 console.log(match.score());
 
-match.pointWonBy("player 1");
+match.pointWonBy('player 1');
 // this will return "0-0, Advantage player 1"
 console.log(match.score());
 
-match.pointWonBy("player 1");
+match.pointWonBy('player 1');
 // this will return "1-0"
 console.log(match.score());
